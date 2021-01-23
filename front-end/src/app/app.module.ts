1cb0c9969fb1ac60ca2329efd219baa83cc07e1e
dqthua <duongquangthuan2002@gmail.com>
2021-01-23 11:12:31 +0700

    up load

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateCustomerComponent } from './Components/create-customer/create-customer.component';
+import { ListCustomerComponent } from './Components/list-customer/list-customer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateCustomerComponent,
+    ListCustomerComponent
   ],
   imports: [
     BrowserModule,
