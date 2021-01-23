4c3d164e2349af108a6f6f64970c785833eb0689
Thanh23198 <nguyendinhthanh2311998@gmail.com>
2021-01-23 11:16:30 +0700

    add room

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -17,3 +17,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('rooms')->group(function (){
+    Route::get('/',[\App\Http\Controllers\RoomController::class,'index']);
+    Route::post('/',[\App\Http\Controllers\RoomController::class,'store']);
+    Route::put('/{id}',[\App\Http\Controllers\RoomController::class,'update']);
+    Route::get('/{id}',[\App\Http\Controllers\RoomController::class,'show']);
+    Route::delete('/id',[\App\Http\Controllers\RoomController::class,'destroy']);
+});
